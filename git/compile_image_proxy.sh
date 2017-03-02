b595f622b67721040fa105632ea48db085255d04
Roger Santos <roger.santos.main@gmail.com>
2017-03-02 17:27:59 +0000

    Create Git docker image

---
+++ git/compile_image_proxy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Verify if you are running as Sudo
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Go to the current file folder
+cd "$(dirname "$0")"
+
+echo "### Git - Image Builder ###"
+
+echo ""
+echo "- build image -"
+docker build -t "rogersantos/git" \
+  --build-arg http_proxy=http://gateway.zscaler.net:9480 \
+  .
+
