75ef3cf9aabb969d35d3420667dd5604fdfcbd24
Roger Santos <roger.santos.main@gmail.com>
2017-03-07 22:18:24 +0000

    Changed the run script to allow seamless proxy configuration

--- vim/run_image.sh
+++ vim/run_image.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
+echo "### Start Vim machine from docker image ###"
+
+# Ensure that run as root
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+  then echo "This program must be run with administrator privileges.  Aborting"
   exit
 fi
 
-echo "### Start Vim machine from docker image ###"
+# Go to the directory where the bash file is
+cd "$(dirname "$0")"
+echo "Running from $(pwd)"
 
-docker run -it --name vim \
-  -v ~/host:/root/host \
-  -v ~/.linux:/root/.linux \
-  rogersantos/vim
+# Exit on any non-zero status.
+trap 'exit' ERR
+set -E
+
+# Get Proxy Configuration
+proxy_url=$(printenv http_proxy | sed 's|http_proxy=||g')
+
+# Run vim docker image
+if [[ ! -z $proxy_url ]]; then
+  echo "Using Proxy: $proxy_url"
+  docker run -it --name vim \
+    -v ~/host:/root/host \
+    -v ~/.linux:/root/.linux \
+    -e http_proxy=$proxy_url \
+    -e https_proxy=$proxy_url \
+    rogersantos/vim
+else
+  docker run -it --name vim \
+    -v ~/host:/root/host \
+    -v ~/.linux:/root/.linux \
+    rogersantos/vim
+fi
+
+echo "#############"
