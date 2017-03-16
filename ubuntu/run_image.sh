1d67812eab3e9806380d40a441a6cb8061e665c8
Roger Santos <roger.santos.main@gmail.com>
2017-03-16 23:00:45 +0000

    Changed ubuntu run_image to consider proxy

--- ubuntu/run_image.sh
+++ ubuntu/run_image.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
+echo "### Ubuntu - Image Runner ###"
+
+# Ensure that run as root
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+  then echo "This program must be run with administrator privileges.  Aborting"
   exit
 fi
 
-echo "### Start Ubuntu machine from docker image ###"
+# Go to the directory where the bash file is
+cd "$(dirname "$0")"
+echo "Running from $(pwd)"
 
-docker run -it --name ubuntu \
-  -v ~/.linux:/root/.linux \
-  -v ~/host:/root/host \
-  rogersantos/ubuntu
+# Exit on any non-zero status.
+trap 'exit' ERR
+set -E
+
+# Get Proxy Configuration
+proxy_url=$(printenv http_proxy | sed 's|http_proxy=||g')
+
+# Run ubuntu docker image
+if [[ ! -z $proxy_url ]]; then
+  echo "Using Proxy: $proxy_url"
+  docker run -it --name ubuntu \
+    -v ~/host:/root/host \
+    -v ~/.linux:/root/.linux \
+    -e http_proxy=$proxy_url \
+    -e https_proxy=$proxy_url \
+    rogersantos/ubuntu
+else
+  docker run -it --name ubuntu \
+    -v ~/host:/root/host \
+    -v ~/.linux:/root/.linux \
+    rogersantos/ubuntu
+fi
+
+echo "#############"
