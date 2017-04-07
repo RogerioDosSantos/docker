5fcb0b1deaa4c3c96c0505c374dc67391f2151c8
Roger Santos <roger.santos.main@gmail.com>
2017-04-07 23:01:57 +0000

    Added docker_registry configuration

---
+++ docker_registry/run_image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "This program must be run with administrator privileges.  Aborting"
+  exit
+fi
+
+echo "### Start Docker Registry machine from docker image ###"
+
+docker run -d --name docker_registry_data \
+  -v /var/lib/registry \
+  rogersantos/alpine
+
+docker run -d --name docker_registry --restart=unless-stopped \
+  -p 5000:5000 \
+  --volumes-from docker_registry_data \
+  rogersantos/docker_registry
