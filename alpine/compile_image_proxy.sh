5b950f283293490f7f9075cf3c0dfeb398469d11
Roger Santos <roger.santos.main@gmail.com>
2017-04-07 23:01:26 +0000

    Added Alpine configuration with bash

---
+++ alpine/compile_image_proxy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "This program must be run with administrator privileges.  Aborting"
+  exit
+fi
+
+#Go to the current file folder
+cd "$(dirname "$0")"
+echo "- Running from $(pwd)"
+
+echo "### Alpine - Image Builder ###"
+
+echo ""
+echo "- build image -"
+docker build -t "rogersantos/alpine" \
+  --build-arg http_proxy=http://gateway.zscaler.net:9480 \
+  .
+
