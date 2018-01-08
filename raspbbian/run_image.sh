05b00f16c923be980691fe0e74b268cb99f2d480
Roger <roger.santos.main@gmail.com>
2018-01-08 09:17:23 -0600

    Removed the need to run as super user

--- raspbbian/run_image.sh
+++ raspbbian/run_image.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "This program must be run with administrator privileges.  Aborting"
-  exit
-fi
-
-
 echo "### Start Raspbian machine from docker image ###"
 
 # Run the qemu multiarch support
